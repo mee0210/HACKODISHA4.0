2c43b0810a9eede0e53c7484341a3b44a1952ec1
Ayesha1209s <ummatulayesha4@gmail.com>
2024-10-21 00:26:05 +0530

    Main.java

---
+++ Main.java
@@ -0,0 +1,35 @@
+public class Main {
+
+	public static void main(String[] args) {
+		LibraryManager libraryManager = new LibraryManager();
+        libraryManager.addBook("1000", "The Catcher in the Rye", "J.D. Salinger", "Fiction");
+        libraryManager.addBook("1001"," Outlander ","Diana Gabaldon"," Romance ");
+        libraryManager.addBook("1002", "The Midnight Library" ,"Matt Haig", "Mental Health");
+        libraryManager.addBook("1003", "Gone Girl", "Gillian Flynn", "Mystery");
+        libraryManager.addBook("1004", "Cosmos", "Carl Sagan ","Science");
+        libraryManager.addBook(" 1005", "On Food and Cooking", "Harold McGee", "Cooking");
+        libraryManager.addBook("1006",  "A Brief History of Time", "Stephen Hawking", " Science");
+        libraryManager.addBook("1007",   "The Da Vinci Code" ," Dan Brown ","Mystery ");
+        libraryManager.addBook("1008"," The Notebook "," Nicholas Sparks", "Romance ");
+        libraryManager.addBook(" 1009" ,"Thirteen Reasons Why", "Jay Asher ", " Mental Health");
+        libraryManager.addBook(" 1010" , " How Baking Works ", " Paula Figoni ", "Cooking ");
+        libraryManager.addBook("1011", "The Selfish Gene" , " Richard Dawkins ", " Science");
+        libraryManager.addBook("1012", " In Cold Blood ", " Truman Capote "," Mystery ");
+        libraryManager.addBook("1013", "Call Me by Your Name","André Aciman","Romance");
+        libraryManager.addBook( "1014", "Every Last Word","Tamara Ireland Stone","Mental Health" );
+        libraryManager.addBook("1015","Culinary Artistry","Andrew Dornenburg & Karen Page","Cooking"    );
+        libraryManager.addBook("1016", "Love, Theoretically","Ali Hazelwood","Romance");
+        libraryManager.addBook("1017", "Silent Spring","Rachel Carson","Science" );
+        libraryManager.addBook(  "1018", "Shutter Island","Dennis Lehane","Mystery" );
+        libraryManager.addBook("1019", "Anxious People","Fredrik Backman","Mental Health" );
+        libraryManager.addBook( "1020","My Bombay Kitchen","Niloufer Ichaporia King","Cooking");
+        libraryManager.addBook( "1021", "Big Bang","Simon Lehna Singh","Science" );
+        libraryManager.addBook(  "1022", "Mine Till Midnight","Lisa Kleypas","Romance" );
+        libraryManager.addBook( "1023", "The Silent Patient","Alex Michaelides","Mystery"   );
+        libraryManager.addBook("1024", "Radio Silence","Alice Oseman","Mental Health");
+        libraryManager.addBook( "1025", "Cleora's Kitchens","Cleora Butler","Cooking");
+        
+        
+	}
+
+}
